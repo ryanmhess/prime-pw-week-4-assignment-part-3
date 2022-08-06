5a9e7060dc096aca0b401494f6d799ad75853dac
Ryan Hess <ryanmhess@gmail.com>
2022-08-06 12:10:47 -0500

    Basic problem seems to be working well for me.  About to start stretch

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,7 +2,15 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
-let basket = [];
+////////////////////////////////////////////////////////////////////////////////////
+//                               GLOBAL VARIABLES                                 //
+////////////////////////////////////////////////////////////////////////////////////
+
+let basket = ['orange'];
+
+////////////////////////////////////////////////////////////////////////////////////
+//                                  FUNCTIONS                                     //
+////////////////////////////////////////////////////////////////////////////////////
 
 function addItem(item){
     console.log(`Adding [${item}] to your basket.`);
@@ -10,23 +18,30 @@ function addItem(item){
     return true;
 }   //  end addItem function
 
+//--------------------------------------------------------------------------------//
+
 function listItems(array){
     console.log('The items in your basket include:');
     for(let i=0; i<array.length; i++){
         console.log(array[i]); //  listing each item in the basket
     }   //  end for loop
-    console.log('test');
+    return '';
 }   //  end listItems function
 
+//--------------------------------------------------------------------------------//
+
 function empty(array){
-    console.log('test 2');
     let itemReset = prompt('\nWould you like to reset your basket?  yes/no\n');
     if(itemReset === 'yes'){
         array = [];
-        console.log('Your basket is now empty:', array);
+        console.log('Your basket is now empty.');
+        console.log('Basket:', array);
+        return '';
     }   //  end if
     else if(itemReset === 'no'){
-        return console.log('No changes made to your basket:', array);
+        console.log('No changes made to your basket.');
+        console.log('Basket:', array);
+        return '';
     }   //  end else if
     else{
         console.log('Invalid Entry.  Please Try Again');
@@ -34,7 +49,9 @@ function empty(array){
     }   //  end else & restart funcion
 }   //  end empty function
 
-//  Tests
+////////////////////////////////////////////////////////////////////////////////////
+//                              TEST COMMANDS                                     //
+////////////////////////////////////////////////////////////////////////////////////
 
 console.log(addItem('apples'));
 console.log(addItem('steak'));
