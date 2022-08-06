31fef142234222403309c1487ad3e4cd23dcab80
Ryan Hess <ryanmhess@gmail.com>
2022-08-06 09:56:56 -0500

    Created the base functions, have not started testing yet.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,22 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+function addItem(item){
+    console.log(`Adding [${item}] to your basket.`);
+    basket.push(item);
+}   //  end addItem function
+
+function listItems(){
+    console.log('The items in your basket include:');
+    for(let i=0; i<basket.length; i++){
+        console.log(basket[i]);
+    }   //  end for loop
+}   //  end listItems function
+
+function empty(){
+
+}   //  end empty function
+
