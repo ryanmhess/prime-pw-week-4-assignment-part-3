12d4ecadb8a0df1fa7884c495e874ebc14ab8b91
Ryan Hess <ryanmhess@gmail.com>
2022-08-06 10:43:34 -0500

    I think I am close to having it done. Still getting some undefined outputs between functions in console though

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -6,17 +6,41 @@ let basket = [];
 
 function addItem(item){
     console.log(`Adding [${item}] to your basket.`);
-    basket.push(item);
+    basket.push(item);  //  adding item to the basket
+    return true;
 }   //  end addItem function
 
-function listItems(){
+function listItems(array){
     console.log('The items in your basket include:');
-    for(let i=0; i<basket.length; i++){
-        console.log(basket[i]);
+    for(let i=0; i<array.length; i++){
+        console.log(array[i]); //  listing each item in the basket
     }   //  end for loop
+    console.log('test');
 }   //  end listItems function
 
-function empty(){
-
+function empty(array){
+    console.log('test 2');
+    let itemReset = prompt('\nWould you like to reset your basket?  yes/no\n');
+    if(itemReset === 'yes'){
+        array = [];
+        console.log('Your basket is now empty:', array);
+    }   //  end if
+    else if(itemReset === 'no'){
+        return console.log('No changes made to your basket:', array);
+    }   //  end else if
+    else{
+        console.log('Invalid Entry.  Please Try Again');
+        empty();
+    }   //  end else & restart funcion
 }   //  end empty function
 
+//  Tests
+
+console.log(addItem('apples'));
+console.log(addItem('steak'));
+console.log(addItem('string cheese'));
+console.log(addItem('chips'));
+
+console.log(listItems(basket));
+
+console.log(empty(basket));
