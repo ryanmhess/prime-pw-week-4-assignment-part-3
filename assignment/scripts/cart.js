0b99b777861cb8fbadb3b4eec9ae7942306e0774
Ryan Hess <ryanmhess@gmail.com>
2022-08-07 10:35:35 -0500

    finished anotherItem function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -9,6 +9,7 @@ console.log('***** Cart Functions *****');
 let basket = ['orange'];
 const maxItems = 5;
 let marker = 0;
+let check = true;
 
 ////////////////////////////////////////////////////////////////////////////////////
 //                                  FUNCTIONS                                     //
@@ -18,12 +19,12 @@ function addItem(array){
     console.log(isFull(array, maxItems, marker));
     if(marker === 0){
         let itemPick = prompt(`[Basket Contents]: ${array}\n\nEnter an item to add to your basket.\n`)
-        console.log(`Adding [${itemPick}] to your basket.`);
+        console.log(`\nAdding [${itemPick}] to your basket.`);
         array.push(itemPick);  //  adding item to the basket
         return true;
     }   //  end if
     else{
-        return '';
+        return false;
     }   //  end else
 }   //  end addItem function
 
@@ -31,41 +32,33 @@ function addItem(array){
 
 function isFull(array, maxItems){
     if(array.length < maxItems){    //  runs if there are less than 5 items
-        console.log(`Item Limit: ${array.length}/5 \n[${array}]\nBasket is full?`);
-        marker = 0;
+        console.log(`\nItem Limit: ${array.length}/5 \n[${array}]\nBasket is full?`);
+        marker = 0; //  marker at 0 will run addItem function
         return false;
     }   //  end if
     else{   //  runs if there are 5 or more items
-        console.log(`Item Limit: ${array.length}/5 \n[${array}]\nBasket is full?`);
-        marker = 1;
+        console.log(`\nItem Limit: ${array.length}/5 \n[${array}]\nBasket is full?`);
+        marker = 1; //  marker at 1 will end addItem function
         return true;
     }   //  end else
 }   //  end isFull function
 
 //--------------------------------------------------------------------------------//
 
-// function addMore(item){
-//     let itemChoice = prompt('\nWould you like to add another item?  [ yes ] / [ no ]\n');
-//     if(itemChoice === 'yes'){
-//         addItem(item);
-//         return '';
-//     }   //  end if & direct to addItem function
-//     else if(itemChoice === 'no'){
-//         listItems(array);
-//         return '';
-//     }   //  end else if & direct to listItems function
-//     else{
-//         console.log('Invalid Entry. Please Try Again');
-//         console.log(' ');
-//         addMore(item);
-//         return '';
-//     }   //  end else & restart funcion
-// }   //  end isFull function
+function anotherItem(array){
+    while(marker === 0){   //  allows you to add more items while basket not full
+        console.log(addItem(array));
+    }   //  end while
+}   //  end anotherItem function
+
+//--------------------------------------------------------------------------------//
+
+
 
 //--------------------------------------------------------------------------------//
 
 function listItems(array){
-    console.log('The items in your basket include:');
+    console.log('\nThe items in your basket include:');
     for(let i=0; i<array.length; i++){
         console.log(array[i]); //  listing each item in the basket
     }   //  end for loop
@@ -99,13 +92,14 @@ function empty(array){
 //                              TEST COMMANDS                                     //
 ////////////////////////////////////////////////////////////////////////////////////
 
-console.log(addItem(basket));
-// console.log(addItem('apples'));
-// console.log(addItem('steak'));
-// console.log(addItem('string cheese'));
-// console.log(addItem('chips'));
-// console.log(' ');
+console.log(addItem(basket));   
+//  runs addItem and isFull functions
+
+console.log(anotherItem(basket));    
+//  based on results from isFull function will re-run above or move to listItems function
 
 console.log(listItems(basket));
+//  just list items in basket
 
 console.log(empty(basket));
+//  tbd
