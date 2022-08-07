c6b6df1e8543129da569acac261838f6d2e97a4f
Ryan Hess <ryanmhess@gmail.com>
2022-08-07 07:21:22 -0500

    got the isFull function working

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -7,16 +7,60 @@ console.log('***** Cart Functions *****');
 ////////////////////////////////////////////////////////////////////////////////////
 
 let basket = ['orange'];
+const maxItems = 5;
+let marker = 0;
 
 ////////////////////////////////////////////////////////////////////////////////////
 //                                  FUNCTIONS                                     //
 ////////////////////////////////////////////////////////////////////////////////////
 
-function addItem(item){
-    console.log(`Adding [${item}] to your basket.`);
-    basket.push(item);  //  adding item to the basket
-    return true;
+function addItem(array){
+    console.log(isFull(array, maxItems, marker));
+    if(marker === 0){
+        let itemPick = prompt(`[Basket Contents]: ${array}\n\nEnter an item to add to your basket.\n`)
+        console.log(`Adding [${itemPick}] to your basket.`);
+        array.push(itemPick);  //  adding item to the basket
+        return true;
+    }   //  end if
+    else{
+        return '';
+    }   //  end else
 }   //  end addItem function
+
+//--------------------------------------------------------------------------------//
+
+function isFull(array, maxItems){
+    if(array.length < maxItems){    //  runs if there are less than 5 items
+        console.log(`Item Limit: ${array.length}/5 \n[${array}]\nBasket is full?`);
+        marker = 0;
+        return false;
+    }   //  end if
+    else{   //  runs if there are 5 or more items
+        console.log(`Item Limit: ${array.length}/5 \n[${array}]\nBasket is full?`);
+        marker = 1;
+        return true;
+    }   //  end else
+}   //  end isFull function
+
+//--------------------------------------------------------------------------------//
+
+// function addMore(item){
+//     let itemChoice = prompt('\nWould you like to add another item?  [ yes ] / [ no ]\n');
+//     if(itemChoice === 'yes'){
+//         addItem(item);
+//         return '';
+//     }   //  end if & direct to addItem function
+//     else if(itemChoice === 'no'){
+//         listItems(array);
+//         return '';
+//     }   //  end else if & direct to listItems function
+//     else{
+//         console.log('Invalid Entry. Please Try Again');
+//         console.log(' ');
+//         addMore(item);
+//         return '';
+//     }   //  end else & restart funcion
+// }   //  end isFull function
 
 //--------------------------------------------------------------------------------//
 
@@ -31,9 +75,9 @@ function listItems(array){
 //--------------------------------------------------------------------------------//
 
 function empty(array){
-    let itemReset = prompt('\nWould you like to reset your basket?  yes/no\n');
+    let itemReset = prompt(`[Basket Contents]: ${array}\n\nWould you like to reset your basket?  [ yes ] / [ no ]\n`);
     if(itemReset === 'yes'){
-        array = [];
+        array = []; //  removes all items in basket
         console.log('Your basket is now empty.');
         console.log('Basket:', array);
         return '';
@@ -44,7 +88,7 @@ function empty(array){
         return '';
     }   //  end else if
     else{
-        console.log('Invalid Entry.  Please Try Again');
+        console.log('Invalid Entry. Please Try Again');
         console.log(' ');
         empty(array);
         return '';
@@ -55,11 +99,12 @@ function empty(array){
 //                              TEST COMMANDS                                     //
 ////////////////////////////////////////////////////////////////////////////////////
 
-console.log(addItem('apples'));
-console.log(addItem('steak'));
-console.log(addItem('string cheese'));
-console.log(addItem('chips'));
-console.log(' ');
+console.log(addItem(basket));
+// console.log(addItem('apples'));
+// console.log(addItem('steak'));
+// console.log(addItem('string cheese'));
+// console.log(addItem('chips'));
+// console.log(' ');
 
 console.log(listItems(basket));
 
